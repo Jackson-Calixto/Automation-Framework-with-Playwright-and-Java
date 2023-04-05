b752c2d3f58af16f57d98c73b752c7a2e1b22f61
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-04-05 20:21:06 -0300

    Other Locators

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -9,15 +9,18 @@ public class FirstTest {
     public void firstTest() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setPermissions(Arrays.asList("geolocation")));
+        BrowserContext browserContext = browser
+                .newContext(new Browser.NewContextOptions().setPermissions(Arrays.asList("geolocation")));
         Page page = browserContext.newPage();
 
         page.navigate("https://bestbuy.com/?intl=nosplash");
-        
-                      //a link [@dataAtribute=''] Http Tag Selector
-        //page.locator("//a[@data-lid='hdr_dotd']").click();
-                            //https://www.w3schools.com/cssref/css_selectors.php
-        page.locator("[data-lid=hdr_dotd]").click(); //CSS Selectors
+
+        // a link [@dataAtribute=''] Http Tag Selector
+        // page.locator("//a[@data-lid='hdr_dotd']").click();
+        // https://www.w3schools.com/cssref/css_selectors.php
+        // page.locator("[data-lid=hdr_dotd]").click(); //CSS Selectors
+        // https://playwright.dev/docs/other-locators
+        page.locator("a:has-text(\"Deal of the Day\")").click(); // Other Locators
 
         System.out.println();
 
