9675c8917b577fc9553ade647232ab5adecb936a
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-04-05 06:50:33 -0300

    Http Tag Selector

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,19 +1,21 @@
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class FirstTest {
 
     @Test
     public void firstTest() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        BrowserContext browserContext = browser.newContext();
-        BrowserContext browserContext2 = browser.newContext();
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setPermissions(Arrays.asList("geolocation")));
         Page page = browserContext.newPage();
-        Page page2 = browserContext2.newPage();
 
-        page.navigate("https://google.com");
-        page2.navigate("https://bing.com");
+        page.navigate("https://bestbuy.com/?intl=nosplash");
+        
+                      //a link [@dataAtribute=''] Http Tag Selector
+        page.locator("//a[@data-lid='hdr_dotd']").click();
 
         System.out.println();
 
